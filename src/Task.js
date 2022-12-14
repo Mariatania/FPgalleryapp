0ff72166c6585a3f4712c9c285e3d8e27a05ea82
Tania <mariatania.renyaan@gmail.com>
2022-12-14 21:14:08 +0700

    coba deh

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Maria Tania" // TODO: replace this
+const studentId = "FE2927838" // TODO: replace this
+
+export { url, studentName, studentId }
